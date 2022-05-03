4deb137de529b3b08b4150cd76aae6337ac9843a
Biribbissolo <104684808+Biribbissolo@users.noreply.github.com>
2022-05-03 15:43:53 +0200

    Update sr.py

--- sr.py
+++ sr.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
                         help='Run either train(training) or val(generation)', default='train')
     parser.add_argument('-gpu', '--gpu_ids', type=str, default=None)
     parser.add_argument('-debug', '-d', action='store_true')
-    parser.add_argument('-enable_wandb', action='store_true')
+    parser.add_argument('-enable_wandb', action=True)  #'store_true'
     parser.add_argument('-log_wandb_ckpt', action='store_true')
-    parser.add_argument('-log_eval', action='store_true')
+    parser.add_argument('-log_eval', action=True)  #'store_true'
 
     # parse configs
     args = parser.parse_args()
